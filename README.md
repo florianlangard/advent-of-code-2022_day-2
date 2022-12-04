4d1065f520824a0f03190893c38062bb475a73a3
florianlangard <florian.langard@gmail.com>
2022-12-04 22:49:12 +0100

    part two done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PHP Advent of code 2022 - Day 2 :christmas_tree: :sparkles:
 
-PHP solution to advent of code 2022 day 2 challenge
+:gift: PHP solution to advent of code 2022 day 2 challenge :gift:
--- advent.php
+++ advent.php
@@ -7,9 +7,13 @@ foreach ($data as $item) {
     $round[] = explode(" ", $item);
 }
 
-$lose = 0;
-$draw = 3;
-$win = 6;
+$lose = 0; // X
+$draw = 3; // Y
+$win = 6; // Z
+
+$rock = 1;
+$paper = 2;
+$scissors = 3;
 
 $score = 0;
 
@@ -57,4 +61,57 @@ foreach ($round as $item) {
         }
     }
 }
+var_dump($score);
+// ================ Part two ==================
 
+// X = lose
+// Y = draw
+// Z = win
+
+$scoreV2 = 0;
+
+foreach ($round as $item) {
+    if ($item[1] === "X") {
+        $scoreV2 += $lose;
+        switch ($round) {
+            case $item[0] === "A":
+                $scoreV2 += $scissors;
+                break;
+            case $item[0] === "B":
+                $scoreV2 += $rock;
+                break;
+            case $item[0] === "C":
+                $scoreV2 += $paper;
+                break;
+        }
+    }
+    if ($item[1] === "Y") {
+        $scoreV2 += $draw;
+        switch ($round) {
+            case $item[0] === "A":
+                $scoreV2 += $rock;
+                break;
+            case $item[0] === "B":
+                $scoreV2 += $paper;
+                break;
+            case $item[0] === "C":
+                $scoreV2 += $scissors;
+                break;
+        }
+    }
+    if ($item[1] === "Z") {
+        $scoreV2 += $win;
+        switch ($round) {
+            case $item[0] === "A":
+                $scoreV2 += $paper;
+                break;
+            case $item[0] === "B":
+                $scoreV2 += $scissors;
+                break;
+            case $item[0] === "C":
+                $scoreV2 += $rock;
+                break;
+        }
+    }
+}
+var_dump($scoreV2);
